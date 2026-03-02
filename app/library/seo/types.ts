2bc244b3abe8ed273d0dac3509487d32b9610b01
Kurnianto Syaputra <Kurnianto.Syaputra@ai.astra.co.id>
2026-03-02 12:58:41 +0700

    update url

--- app/library/seo/types.ts
+++ app/library/seo/types.ts
@@ -2,4 +2,4 @@ export const SITE_NAME = "RS Sehat";
 export const DEFAULT_OG = "/og.png";
 export const SUPPORTED_LOCALES = ["id", "en"];
 export const SITE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_SITE_URL || "https://hospital-landing-chi.vercel.app";
